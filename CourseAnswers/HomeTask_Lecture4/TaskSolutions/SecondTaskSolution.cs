7f71b3989321a37ea6e8b83f36a4e8f3dafef727
bosak-dmitry <dmitry.bosak@itomy.ch>
2022-10-21 11:17:26 +0300

    lecture-4: additional description

--- CourseAnswers/HomeTask_Lecture4/TaskSolutions/SecondTaskSolution.cs
+++ CourseAnswers/HomeTask_Lecture4/TaskSolutions/SecondTaskSolution.cs
@@ -15,7 +15,7 @@
 
                 if (firstNumber % 2 == 0 && secondNumber % 2 == 0 ||
                     firstNumber % 2 != 0 && secondNumber % 2 != 0)
-                    numbers.Insert(i++ +1, 0);
+                    numbers.Insert(i++ +1, 0); // тут сдвигаем индекс вперёд, чтоб не вставлять 0 между только что вставленным нулём и другими числами
             }
 
             Console.WriteLine($"Result: {string.Join(", ", numbers)}");
